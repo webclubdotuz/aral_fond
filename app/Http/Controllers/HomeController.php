44513420e0a324b9c9099196deb936d144b6335f
Mambetali Nokisbaev <69534775+webclubdotuz@users.noreply.github.com>
2023-04-07 18:26:15 +0500

    asda

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Job;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('home');
+
+        $ball_null = 0;
+        $ball_is = 0;
+
+        if(hasRole('admin'))
+        {
+            $ball_is = Job::where('ball', '!=', null)->where('status', 'active')->count();
+            $ball_null = Job::where('ball', null)->where('status', 'active')->count();
+        }else{
+            $ball_is = Job::where('ball', '!=', null)->where('status', 'active')->where('user_id', auth()->user()->id)->count();
+            $ball_null = Job::where('ball', null)->where('status', 'active')->where('user_id', auth()->user()->id)->count();
+        }
+
+        return view('home', compact('ball_null', 'ball_is'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,4 +4,19 @@
 
 @section('content')
 
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-body">
+                <h1 class="text-success">
+                    Балл қойылған: {{ $ball_is }}
+                </h1>
+                <h1 class="text-danger">
+                    Балл қойылмаган: {{ $ball_null }}
+                </h1>
+            </div>
+        </div>
+    </div>
+</div>
+
 @endsection
